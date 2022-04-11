3a17db4744ca79ffb3339a9677b2699636c05a98
Mafahes <vadm11.12@mail.ru>
2022-04-11 17:19:24 +0300

    upd

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -76,7 +76,7 @@ class _SettingsPageState extends State<SettingsPage> {
                         Text(
                           'Настройки', style: TextStyle(color: Colors.white, fontFamily: 'Lato', fontSize: 20.sp, fontWeight: FontWeight.bold),),
                         SizedBox(height: 4),
-                        Text(' (v1.8)', style: TextStyle(color: Colors.grey, fontSize: 8),)
+                        Text(' (v2.0)', style: TextStyle(color: Colors.grey, fontSize: 8),)
                       ],
                     ),
                   )
